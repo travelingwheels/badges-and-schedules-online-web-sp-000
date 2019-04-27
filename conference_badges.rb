4c14aaf12b817512a6437b8b39eddd971860ec60
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-27 23:49:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,5 +7,5 @@ end
 
 def batch_badge_creator(attendees)
   attendees = ["Edsger", "Ada","Charles", "Alan", "Grace", "Linus", "Matz"]
-  puts "Hello, my name is #{attendees}."
+  return "Hello, my name is #{attendees}."
 end
