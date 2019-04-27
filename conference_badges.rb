3af5b59c51f5e5340e2bddbb274645c2531e28d8
Gary Wheeler <gkwheeler40@gmail.com>
2019-04-27 22:06:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 def badge_maker(name)
   name = ["Arel", "Edsger", "Ada","Charles", "Alan", "Grace", "Linus", "Matz"]
   name.each do |name|
-     return "Hello, my name is #{name}"
+     return "Hello, my name is #{name}."
   end
 end
